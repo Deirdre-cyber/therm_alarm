65bbb810a1a6ec5ab8691e55ee0a5cbb6e73bfca
Deirdre Laptop <dleeee333@gmail.com>
2024-04-04 11:11:37 +0100

    Added try catches to resolve messy exit on terminal exit and cover port issues

--- communicate_with_microbit.py
+++ communicate_with_microbit.py
@@ -1,16 +1,29 @@
 import serial
 import sys
 
+COM_PORT = 'COM3'
 BAUD_RATE = 115200
 
-ser = serial.Serial('COM3', BAUD_RATE)
 
-if ser.is_open:
-    print("Serial port opened")
-else:
-    print("Could not open serial port.")
+
+try:
+    ser = serial.Serial(COM_PORT, BAUD_RATE)
+
+    if ser.is_open:
+        print("Serial port opened")
+    else:
+        print("Could not open serial port.")
+        sys.exit()
+
+    while True:
+        try:
+            temperature_data = ser.readline().decode('utf-8').rstrip()
+            print("Data received:", temperature_data)
+        except KeyboardInterrupt as e:
+            print("Keyboard interrupt. Exiting program")
+            ser.close()
+            sys.exit()
+
+except serial.SerialException as e:
+    print("Serial port error. Exiting program")
     sys.exit()
-
-while True:
-    temperature_data = ser.readline().decode('utf-8').rstrip()
-    print("Data received:", temperature_data)

2872027c86f273b1039b65b03c1ffd2269266016
Deirdre Laptop <dleeee333@gmail.com>
2024-04-05 10:52:00 +0100

    Brought in senor.py to project. updated recevier to correctly parse and store data being received

--- communicate_with_microbit.py
+++ communicate_with_microbit.py
@@ -1,10 +1,9 @@
 import serial
 import sys
+from datetime import datetime
 
 COM_PORT = 'COM3'
 BAUD_RATE = 115200
-
-
 
 try:
     ser = serial.Serial(COM_PORT, BAUD_RATE)
@@ -15,14 +14,46 @@ try:
         print("Could not open serial port.")
         sys.exit()
 
-    while True:
-        try:
-            temperature_data = ser.readline().decode('utf-8').rstrip()
-            print("Data received:", temperature_data)
-        except KeyboardInterrupt as e:
-            print("Keyboard interrupt. Exiting program")
-            ser.close()
-            sys.exit()
+    # log file is also for debugging
+    with open('temperature_data.txt', 'w') as log_file:
+        while True:
+            try:
+                temperature_data = ser.readline().decode('utf-8').rstrip()
+                print("Data received:", temperature_data)
+
+                variables = temperature_data.split(',')
+
+                current_time = datetime.now()
+
+                # dictionary for the date for ease in uploading to the database
+                data = {
+                    'sensor_id' : variables[0],
+                    'date_time' : current_time,
+                    'max_threshold' : variables[1],
+                    'min_threshold' : variables[2],
+                    'location_id' : variables[3],
+                    'curr_temp' : variables[4],
+                    'is_exceeded' : variables[5]
+                }
+
+                print("Number of variables:", len(variables)) # debug
+
+                # following print statements all for debugging purposes
+                print("sensor_id:", data['sensor_id'])
+                print("date_time:", data['date'])
+                print("max_threshold:", data['max_threshold'])
+                print("min_threshold:", data['min_threshold'])
+                print("location_id:", data['location_id'])
+                print("curr_temp:", data['curr_temp'])
+                print("is_exceeded:", data['is_exceeded'])
+
+                # log file is also for debugging
+                log_file.write(temperature_data + '\n')
+
+            except KeyboardInterrupt as e:
+                print("Keyboard interrupt. Exiting program")
+                ser.close()
+                sys.exit()
 
 except serial.SerialException as e:
     print("Serial port error. Exiting program")
---
+++ sensor.py
@@ -0,0 +1,32 @@
+from microbit import *
+import utime
+
+SENSOR_ID = "SENSE001"
+LOCATION_ID = "BEDROOM_MAIN"
+HIGH_THRESHOLD = 23
+LOW_THRESHOLD = 19
+
+
+with open("temperature_log.txt", "w") as log_file:
+
+    while True:
+        curr_temp = temperature()
+        is_exceeded = False
+
+
+        if curr_temp > HIGH_THRESHOLD or curr_temp < LOW_THRESHOLD:
+            is_exceeded = True
+            display.show(Image.SAD)
+
+        else:
+            display.show(Image.HAPPY)
+
+        log_data = "{}, {}, {}, {}, {}, {}".format(
+            SENSOR_ID, HIGH_THRESHOLD, LOW_THRESHOLD, LOCATION_ID, curr_temp, is_exceeded)
+
+        sleep(2000)
+
+        print(log_data)
+        log_file.write(log_data)
+
+        sleep(5000)
